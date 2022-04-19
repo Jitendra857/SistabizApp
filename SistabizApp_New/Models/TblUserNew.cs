980b10253fea2b537b451141be8143ce8a91c23a
Jitendra857 <joshijitu9587@gmail.com>
2022-04-19 13:41:46 +0530

    Initial Changes

---
+++ SistabizApp_New/Models/TblUserNew.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistabizApp_New.Models
+{
+    public partial class TblUserNew
+    {
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SistabizApp_New/Models/Tbluser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistabizApp_New.Models
+{
+    public partial class Tbluser
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
